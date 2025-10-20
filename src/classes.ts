840fe8dce2beff8682d56eb61109b591c74fcbbd
Nate Mosco <natemosco@gmail.com>
2025-10-20 01:38:42 -0400

    change class structure for storing history

--- src/classes.ts
+++ src/classes.ts
@@ -3,21 +3,27 @@
 // Each session should be able to store data about multiple games played within that session.
 // The history should be accessible throughout the app so that each game can log its results to the session history.
 // If possible, I want to implement this using the singleton pattern so that there's only one instance of SessionHistory throughout the app.
-interface GameSession{
+export interface GameSession{
     gameName: string;
     player: string;
     result: string;
     timestamp: Date;
 };
-interface TicTacToeScoreboard {
+export interface TicTacToeScoreboard {
     gameName: string;
     playerX: number;
     playerO: number;
     tieMatches: number;
 }
+type Games = "Tic Tac Toe" | "Generic Game";
 class SessionHistory {
     private static instance: SessionHistory;
-    private gameSessions: (GameSession|TicTacToeScoreboard)[] = [];
+    //this needs to be an object that can store different types of game session data
+
+    private gameSessions: Record <Games, GameSession[] | TicTacToeScoreboard >= {
+        "Tic Tac Toe": {gameName: "Tic Tac Toe", playerX: 0, playerO: 0, tieMatches: 0},
+        "Generic Game": []
+    };
 
     private constructor() {
         // Private constructor to prevent instantiation
@@ -31,9 +37,24 @@ class SessionHistory {
         }
         return SessionHistory.instance;
     }
+
+    // type guard to determine what type of game session data is being logged
+    private isGameSessionArray(value: GameSession[] | TicTacToeScoreboard): value is GameSession[] {
+        return Array.isArray(value);
+    }
+    
     public logGameSessionHistory(history: GameSession|TicTacToeScoreboard): void {
-        this.gameSessions.push(history);
+        switch(history.gameName){
+            case "Tic Tac Toe":
+                this.gameSessions["Tic Tac Toe"] = history as TicTacToeScoreboard;
+                break;
+            default:
+                const genericGameSessions = this.gameSessions["Generic Game"];
+                if(this.isGameSessionArray(genericGameSessions)){
+                    genericGameSessions.push(history as GameSession)
+                }
+        }
     }
 }
 
-export type { SessionHistory, GameSession, TicTacToeScoreboard };
+export { SessionHistory };
