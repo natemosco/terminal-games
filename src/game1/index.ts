40e6b9c3f0d36a46ef6a92103af7ebc3076b8f31
Nate Mosco <natemosco@gmail.com>
2025-10-20 02:02:51 -0400

    update tic tac toe game logic to replay once completed

--- src/game1/index.ts
+++ src/game1/index.ts
@@ -1,12 +1,14 @@
-import { TicTacToeScoreboard } from "../classes";
+import { SessionHistory, TicTacToeScoreboard } from "../classes";
 
 function ticTacToe(): void {
     console.log("Welcome to Tic Tac Toe!");
     // Game logic would go here
     
     let currentPlayer: 'X' | 'O' = 'X';
-    let scoreBoard: TicTacToeScoreboard = { gameName: 'Tic Tac Toe', playerX: 0, playerO: 0, tieMatches: 0 };
-    let board: string[][] = [
+    let scoreBoard: TicTacToeScoreboard = SessionHistory.getInstance().getSessionHistory("Tic Tac Toe") as TicTacToeScoreboard;
+    type Row = [string, string, string];
+    type Board = [Row, Row, Row];
+    let board: Board = [
         [' ', ' ', ' '],
         [' ', ' ', ' '],
         [' ', ' ', ' ']
@@ -40,7 +42,10 @@ function ticTacToe(): void {
     }
 
     function printBoard(): void {
-        console.log("Current Board:/n")
+        console.log(`Current turn: Player: ${currentPlayer}. Turn number: ${turnCounter + 1}, /n Scoreboard: 
+            Player X: ${scoreBoard.playerX}, Player O: ${scoreBoard.playerO}, Ties: ${scoreBoard.tieMatches}`);
+        console.log('--------------------------------------------------------------');
+        console.log("Current Board:/n/n")
         board.forEach((row, index) =>{
             console.log(`row: ${index} `,row.join(' | '))
             console.log('--------------')
@@ -48,8 +53,43 @@ function ticTacToe(): void {
     }
 
 
-    
+    while( !forfeit && turnCounter <9 ){
+        printBoard();
+        const prompt = require('prompt-sync')();
+        const move = prompt(`Player ${currentPlayer}, enter your move as row,col (or type 'forfeit' or 'f' to forfeit): `);
+        if(move.toLowerCase() === 'forfeit' || move.toLowerCase() === 'f'){
+            console.log("Forfeiting the game gives the win to the other player.")
+            prompt("Enter 'y' to confirm forfeiting, or any other key to continue playing: ");
+            if(prompt.toLowerCase() === 'y'){
+                foreitGame(currentPlayer);
+                break;
+            }
+            else {
+                continue;
+            } 
+        }
+        const [row, col] = move.split(',').map((num: string)=> parseInt(num.trim()));
+        if( isNaN(row) || isNaN(col)  || row < 0 || row > 2 || col < 0 || col > 2 ){
+            console.log("Invalid move. Please enter row and column as numbers between 0-2 in the following format: row,column i.e. 0,1");
+                continue;
+        }
+        else if( board[row]![col] !== ' ' ){
+            console.log("This spot is already taken. Please choose another spot.");
+            continue;
+        }
+        else{
+            board[row]![col] = currentPlayer;
+            turnCounter += 1;
+            currentPlayer = (currentPlayer) === 'X'? 'O' : 'X'
+        }
+    }
+    SessionHistory.getInstance().logGameSessionHistory(scoreBoard);
 
+    let anotherGamePrompt = require('prompt-sync')();
+    const playAgain = anotherGamePrompt("Game over! Would you like to play another game of Tic Tac Toe? (y/n): ");
+    if(playAgain.toLowerCase() === 'y'){
+        ticTacToe();
+    }
     
     return;
 };
